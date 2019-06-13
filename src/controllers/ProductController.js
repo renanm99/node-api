ebd8168d02391c7b8f1312560637f10079895257
Renan Oliveira <renanoliveira2199@gmail.com>
2019-06-13 00:28:59 -0300

    send

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,19 +5,19 @@ module.exports = {
     async index(req, res) {
         const { page = 1 } = req.query;
         const products = await Product.paginate({}, { page, limit: 10 });
-        return res.send(products);
+        res.send(products);
     },
 
     async show(req, res) {
         const product = await Product.findById(req.params.id);
 
-        return res.send(product);
+        res.send(product);
     },
 
     async store(req, res) {
         const product = await Product.create(req.body);
 
-        return res.send(product);
+        res.send(product);
     },
 
     async update(req, res) {
@@ -27,14 +27,14 @@ module.exports = {
             { new: true }
         );
 
-        return res.send(product);
+        res.send(product);
     },
     async destroy(req, res) {
         await Product.findOneAndRemove(req.params.id);
 
-        return res.send();
+        res.send();
     },
     async root(req, res) {
-        return res.sendFile('/views/index.html',{root: "./src"});
+        res.sendFile('/views/index.html',{root: "./src"});
     }
 };
