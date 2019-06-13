ed686e364d4e4501a2af11c40a336b80fc55fb32
Renan Oliveira <renanoliveira2199@gmail.com>
2019-06-12 23:16:05 -0300

    bbb

--- server.js
+++ server.js
@@ -10,12 +10,8 @@ app.use(cors());
 
 // Iniciando o DB
 
-mongoose.connect(
-    "mongodb+srv://nodeapi:lota78199Z@cluster0aula-93nfw.mongodb.net/nodeapi?retryWrites=true&w=majority",
-    {
-        useNewUrlParser: true
-    }
-);
+mongoose.connect("mongodb+srv://nodeapi:lota78199Z@cluster0aula-93nfw.mongodb.net/nodeapi?retryWrites=true&w=majority",{useNewUrlParser: true});
+
 requireDir("./src/models");
 
 const Product = mongoose.model("Product");
